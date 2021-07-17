ec07f5179de5070cc78f438a459418bd25896881
railsonsousa106 <railsonsousa106@gmail.com>
2021-07-17 12:39:44 -0400

    update

--- framework/saleor/api/operations/get-all-products.ts
+++ framework/saleor/api/operations/get-all-products.ts
@@ -30,7 +30,7 @@ export default function getAllProductsOperation({
     const { fetch, locale } = commerce.getConfig(config)
 
     if (featured) {
-      variables = { ...variables, categoryId: 'Q29sbGVjdGlvbjo0' };
+      variables = { ...variables, categoryId: 'Q29sbGVjdGlvbjox' };
       query = Query.CollectionOne
     } 
 
